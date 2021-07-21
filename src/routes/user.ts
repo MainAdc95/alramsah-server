cdde404adda5699d363e49d1dce0c49d6ffeb5e3
mustafa <mustafamohammedalhallaq@gmail.com>
2021-07-22 03:55:39 +0400

    SDF

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,16 +6,16 @@ import {
     editRole,
 } from "../handlers/user";
 import { isLoggedIn } from "../middlewares/auth";
-import { isSuperAdmin } from "../middlewares/roles";
+import { isAdmin } from "../middlewares/roles";
 
 const router = express.Router();
 
 router.get("/users", isLoggedIn, getUsers);
 
-router.post("/user_role/:userId", isLoggedIn, isSuperAdmin, manageUserRole);
+router.post("/user_role/:userId", isLoggedIn, isAdmin, manageUserRole);
 
 router.put("/profile", isLoggedIn, editProfile);
 
-router.put("/user/:userId/user_role", isLoggedIn, isSuperAdmin, editRole);
+router.put("/user/:userId/user_role", isLoggedIn, isAdmin, editRole);
 
 export default router;
