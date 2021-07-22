595d9ac695cc99d75415fc06069cba3b520b12be
mustafa <mustafamohammedalhallaq@gmail.com>
2021-07-22 19:23:40 +0400

    DSF

--- src/routes/image.ts
+++ src/routes/image.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { addImages, getImages, deleteImage } from "../handlers/image";
 import { isLoggedIn } from "../middlewares/auth";
-import { isAdmin } from "../middlewares/roles";
+import { isAdmin, isEditor } from "../middlewares/roles";
 
 const router = express.Router();
 
 router.get("/images", getImages);
 
-router.post("/images", isLoggedIn, isAdmin, addImages);
+router.post("/images", isLoggedIn, isEditor, addImages);
 
 router.delete("/image/:imageId", isLoggedIn, isAdmin, deleteImage);
 
